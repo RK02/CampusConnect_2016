ad687178a8fd2f72383c8d94e0ffe1d0ca4c550b
Rakesh <rk211195@gmail.com>
2016-07-26 19:43:47 +0530

    Before pr 16 merge

--- app/src/main/java/com/campusconnect/auxiliary/IntroPageTransformer.java
+++ app/src/main/java/com/campusconnect/auxiliary/IntroPageTransformer.java
@@ -15,8 +15,8 @@ public class IntroPageTransformer implements ViewPager.PageTransformer {
 
         int pagePosition = (int) page.getTag();
 
-        int pageWidth = page.getWidth();
-        float pageWidthTimesPosition = pageWidth * position;
+        int pageHeight = page.getHeight();
+        float pageHeightTimesPosition = pageHeight * position;
         float absPosition = Math.abs(position);
 
         if (position <= -1.0f || position >= 1.0f) {
@@ -27,18 +27,18 @@ public class IntroPageTransformer implements ViewPager.PageTransformer {
 
             View title = page.findViewById(R.id.title);
             title.setAlpha(1.0f - absPosition);
-            title.setTranslationX(-pageWidthTimesPosition);
-            title.setTranslationY(pageWidthTimesPosition*1.2f);
+            title.setTranslationX(-pageHeightTimesPosition);
+            title.setTranslationY(pageHeightTimesPosition*1.2f);
 
             View description = page.findViewById(R.id.description);
-            description.setTranslationX(-pageWidthTimesPosition);
-            description.setTranslationY(pageWidthTimesPosition*1.0f);
+            description.setTranslationX(-pageHeightTimesPosition);
+            description.setTranslationY(pageHeightTimesPosition*1.0f);
             description.setAlpha(1.0f - absPosition);
 
             View computer = page.findViewById(R.id.iv_walkthrough);
             computer.setAlpha(1.0f - absPosition);
-            computer.setTranslationX(-pageWidthTimesPosition);
-            computer.setTranslationY(pageWidthTimesPosition*1.5f);
+            computer.setTranslationX(-pageHeightTimesPosition);
+            computer.setTranslationY(pageHeightTimesPosition*1.4f);
 
             if (pagePosition == 0 && computer != null) {
             }
